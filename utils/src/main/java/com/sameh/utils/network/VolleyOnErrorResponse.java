6b152d5efee4543e065655d0c4fc8acb448d80d3
SamehZizo <samh_zamalkawe@hotmail.com>
2018-10-17 14:52:05 +0200

    1.0.5

--- utils/src/main/java/com/sameh/utils/network/VolleyOnErrorResponse.java
+++ utils/src/main/java/com/sameh/utils/network/VolleyOnErrorResponse.java
@@ -1,6 +1,7 @@
 package com.sameh.utils.network;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.widget.Toast;
 import com.android.volley.Response;
@@ -13,19 +14,38 @@ public class VolleyOnErrorResponse implements Response.ErrorListener {
     private Context context;
     private CustomProgressDialog customProgressDialog;
     private boolean logError;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
-    public VolleyOnErrorResponse(Context context , CustomProgressDialog customProgressDialog , boolean logErrror) {
+    public VolleyOnErrorResponse(Context context, SwipeRefreshLayout swipeRefreshLayout, boolean logError) {
+        this.context = context;
+        this.logError = logError;
+        this.swipeRefreshLayout = swipeRefreshLayout;
+    }
+
+    public VolleyOnErrorResponse(Context context , CustomProgressDialog customProgressDialog , boolean logError) {
         this.context = context;
         this.customProgressDialog = customProgressDialog;
-        this.logError = logErrror;
+        this.logError = logError;
+    }
+
+    public VolleyOnErrorResponse(Context context, CustomProgressDialog customProgressDialog, SwipeRefreshLayout swipeRefreshLayout, boolean logError) {
+        this.context = context;
+        this.logError = logError;
+        this.swipeRefreshLayout = swipeRefreshLayout;
+        this.customProgressDialog = customProgressDialog;
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-        customProgressDialog.cancel();
         Toast.makeText(context, R.string.loading_data_error, Toast.LENGTH_SHORT).show();
 
         if (logError)
             Log.d("Volley Error",error.getMessage());
+
+        if (customProgressDialog != null)
+            customProgressDialog.cancel();
+
+        if (swipeRefreshLayout != null)
+            swipeRefreshLayout.setRefreshing(false);
     }
 }
